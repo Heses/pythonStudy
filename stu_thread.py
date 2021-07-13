e201e1274c719671d6e0be886960a113d6fd42e8
hsese <dbqls01477@naver.com>
2021-07-13 22:36:54 +0900

    OOP 멀티스레드 실습

---
+++ stu_thread.py
@@ -0,0 +1,23 @@
+#Thread : 프로그램 하나에서 여러 작업을 동시에 처리하도록 하는 기능
+#Multi Thread : 여러 개의 스레드를 작동하는 것
+
+import time
+
+class RaceCar:
+    carname=''
+    def __init__(self,name):
+        self.carname=name
+
+    def runcar(self):
+        for _ in range(0,3): #변수 없이 for문 돌리려면 _ 사용, 띄어쓰기 주의
+            carStr=self.carname+'~~running.\n'
+            print(carStr,end='')
+            time.sleep(0.1) #0.1sec stop
+
+car1=RaceCar('자동차1')
+car2=RaceCar('자동차2')
+car3=RaceCar('자동차3')
+
+car1.runcar()
+car2.runcar()
+car3.runcar()
